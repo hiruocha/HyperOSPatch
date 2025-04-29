bdaf48aec4493848c8ebf3c362202ec81574cdae
hiruocha <hiruocha@gmail.com>
2025-04-29 18:30:40 +0800

    feat: 去除国行系统GMS限制
    修改自 https://github.com/fei-ke/unlock-cn-gms

---
+++ script/unlock-cn-gms.sh
@@ -0,0 +1,14 @@
+ui_print "正在去除国行系统GMS限制……"
+file=/product/etc/permissions/cn.google.services.xml
+if [ -e "$file" ]
+then
+    ui_print "已找到文件$file，修改中……"
+    target=$MODPATH/system$file
+    mkdir -p $(dirname $target)
+    cp -f $file $target
+    sed -i '/cn.google.services/d' $target
+    sed -i '/services_updater/d' $target
+    ui_print "成功！"
+else
+    ui_print "未找到$file！"
+fi
