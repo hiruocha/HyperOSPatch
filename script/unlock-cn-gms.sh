4cdddcbfb1b22047f6abe7fead08581b1714dbde
hiruocha <hiruocha@gmail.com>
2025-04-29 19:48:08 +0800

    refactor(unlock-cn-gms.sh): 优化脚本

--- script/unlock-cn-gms.sh
+++ script/unlock-cn-gms.sh
@@ -1,12 +1,12 @@
 file=/product/etc/permissions/cn.google.services.xml
 if [ -e "$file" ]
 then
-    ui_print "已找到文件$file，修改中……"
-    target=$MODPATH/system$file
-    mkdir -p $(dirname $target)
-    cp -f $file $target
-    sed -i '/cn.google.services/d' $target
-    sed -i '/services_updater/d' $target
+    ui_print "已找到文件 $(basename $file)，修改中……"
+    target="$MODPATH/system$file"
+    mkdir -p $(dirname "$target")
+    cp -f "$file" "$target"
+    sed -i '/cn.google.services/d' "$target"
+    sed -i '/services_updater/d' "$target"
     ui_print "成功！"
 else
     ui_print "未找到文件！"
