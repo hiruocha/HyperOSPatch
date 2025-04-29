d77fb80b6ac41a8a568c016c9bb8cc9aaf2b8f2b
hiruocha <hiruocha@gmail.com>
2025-04-30 03:17:21 +0800

    style(unlock-cn-gms.sh): 调整输出

--- script/unlock-cn-gms.sh
+++ script/unlock-cn-gms.sh
@@ -1,14 +1,14 @@
-ui_print "正在去除国行系统GMS限制……"
+ui_print "- 正在去除国行系统GMS限制……"
 file=/product/etc/permissions/cn.google.services.xml
 if [ -e "$file" ]
 then
-    ui_print "已找到文件 $(basename $file)，修改中……"
+    ui_print "- 已找到文件 $(basename $file)，修改中……"
     target="$MODPATH"/system"$file"
     mkdir -p $(dirname "$target")
     cp -f "$file" "$target"
     sed -i '/cn.google.services/d' "$target"
     sed -i '/services_updater/d' "$target"
-    ui_print "成功！"
+    ui_print "- 成功！"
 else
-    ui_print "未找到文件！"
+    ui_print "- 未找到文件！"
 fi
