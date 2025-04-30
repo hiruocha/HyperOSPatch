d2c5019475dda3ab423c8c5cb3fcd18d6425317b
hiruocha <hiruocha@gmail.com>
2025-05-01 07:06:21 +0800

    fix(script): 修复模块更新逻辑

--- script/enable_video_depth.sh
+++ script/enable_video_depth.sh
@@ -1,14 +1,14 @@
 ui_print "- 正在添加视频景深壁纸……"
 file=/product/media/wallpaper/sysui_editor_wallpaper_group/enable_video_depth
-if [ -e "$file" ]
-then
-    ui_print "- 已找到文件 $(basename $file)，无需修改！"
-elif [ -e "$OLD_MODPATH/system$file" ]
+if [ -e "$OLD_MODPATH/system$file" ]
 then
     ui_print "- 已找到旧模块文件 $(basename $file)，复制中……"
     mkdir -p "$(dirname "$MODPATH/system$file")"
     cp "$OLD_MODPATH/system$file" "$MODPATH/system$file"
     ui_print "- 成功！"
+elif [ -e "$file" ]
+then
+    ui_print "- 已找到文件 $(basename $file)，无需修改！"
 elif getprop ro.build.version.incremental | grep -q '^OS2'
 then
     ui_print "- 未找到文件 $(basename $file)，创建中……"
--- script/unlock_cn_gms.sh
+++ script/unlock_cn_gms.sh
@@ -1,6 +1,12 @@
 ui_print "- 正在去除国行系统GMS限制……"
 file=/product/etc/permissions/cn.google.services.xml
-if [ -e "$file" ]
+if [ -e "$OLD_MODPATH/system$file" ]
+then
+    ui_print "- 已找到旧模块文件 $(basename $file)，复制中……"
+    mkdir -p "$(dirname "$MODPATH/system$file")"
+    cp "$OLD_MODPATH/system$file" "$MODPATH/system$file"
+    ui_print "- 成功！"
+elif [ -e "$file" ]
 then
     ui_print "- 已找到文件 $(basename $file)，修改中……"
     target="$MODPATH/system$file"
@@ -8,12 +14,6 @@ then
     cp -f "$file" "$target"
     sed -i '/cn.google.services/d' "$target"
     sed -i '/services_updater/d' "$target"
-    ui_print "- 成功！"
-elif [ -e "$OLD_MODPATH/system$file" ]
-then
-    ui_print "- 已找到旧模块文件 $(basename $file)，复制中……"
-    mkdir -p "$(dirname "$MODPATH/system$file")"
-    cp "$OLD_MODPATH/system$file" "$MODPATH/system$file"
     ui_print "- 成功！"
 else
     ui_print "- 未找到文件，可能是非国行系统！"
