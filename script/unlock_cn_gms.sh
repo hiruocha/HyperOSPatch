3ffb4e5c5ad11e8dbae1aa2a5d48e25ac7bb487c
hiruocha <hiruocha@gmail.com>
2025-05-02 04:51:13 +0800

    style(script): 统一变量写法

--- script/unlock_cn_gms.sh
+++ script/unlock_cn_gms.sh
@@ -1,15 +1,15 @@
 ui_print "- 正在去除国行系统 GMS 限制……"
 file=/product/etc/permissions/cn.google.services.xml
-if [ -e "$OLD_MODPATH/system$file" ]
+if [ -e "$OLD_MODPATH"/system"$file" ]
 then
-    ui_print "- 已找到旧模块文件 $(basename $file)，复制中……"
-    mkdir -p "$(dirname "$MODPATH/system$file")"
-    cp "$OLD_MODPATH/system$file" "$MODPATH/system$file"
+    ui_print "- 已找到旧模块文件 $(basename "$file")，复制中……"
+    mkdir -p "$(dirname "$MODPATH"/system"$file")"
+    cp "$OLD_MODPATH"/system"$file" "$MODPATH"/system"$file"
     ui_print "- 成功！"
 elif [ -e "$file" ]
 then
-    ui_print "- 已找到文件 $(basename $file)，修改中……"
-    target="$MODPATH/system$file"
+    ui_print "- 已找到文件 $(basename "$file")，修改中……"
+    target="$MODPATH"/system"$file"
     mkdir -p $(dirname "$target")
     cp -f "$file" "$target"
     sed -i '/cn.google.services/d' "$target"
