be36f5ef257a83dd57874f4b01db836dcfd5359f
hiruocha <hiruocha@gmail.com>
2025-05-21 04:58:00 +0800

    feat: 添加 zram 1:1 白名单

--- module/customize.sh
+++ module/customize.sh
@@ -1,8 +1,15 @@
 ui_print "- 开始安装……"
 
 OLD_MODPATH=/data/adb/modules/hyperos_patch
-CODENAME=$(getprop ro.product.name)
+PRODUCT_NAME=$(getprop ro.product.name)
 VERSION=$(getprop ro.mi.os.version.name)
+
+for BIN in "$MODPATH"/bin/*
+do
+    chmod +x "$BIN"
+done
+
+JQ="$MODPATH"/bin/jq
 
 for script in "$MODPATH"/script/*.sh
 do
@@ -11,3 +18,4 @@ done
 
 ui_print "- 正在清理……"
 rm -rf "$MODPATH"/script
+rm -rf "$MODPATH"/bin
---
+++ script/zram_whitelist.sh
@@ -0,0 +1,43 @@
+ui_print "- 正在添加 zram 1:1 白名单……"
+
+FILE=/system_ext/etc/perfinit_bdsize_zram.conf
+
+if [ -e "$OLD_MODPATH"/system"$FILE" ]
+then
+    ui_print "- 已找到旧模块文件 $(basename "$FILE")，复制中……"
+    mkdir -p "$(dirname "$MODPATH"/system"$FILE")"
+    cp "$OLD_MODPATH"/system"$FILE" "$MODPATH"/system"$FILE"
+    ui_print "- 成功！"
+elif ! echo "$VERSION" | grep -q '^OS2'
+then
+    ui_print "- 非 HyperOS 2.0！"
+    ui_print "- 跳过修改！"
+elif [ -e "$FILE" ]
+then
+    ui_print "- 已找到文件 $(basename "$FILE")，修改中……"
+    TARGET="$MODPATH"/system"$FILE"
+    mkdir -p "$(dirname "$TARGET")"
+    cp -f "$FILE" "$TARGET"
+    if "$JQ" -e --arg productname "$PRODUCT_NAME" '.zram[] | select(.product_name[] | contains($productname))' "$TARGET" > /dev/null
+    then
+        ui_print "- 已存在白名单中!"
+        ui_print "- 跳过修改！"
+    else
+        ZRAM_SIZE='{"2": 2048, "3": 3072, "4": 4096, "6": 6144, "8": 8192, "10": 10240, "12": 12288, "16": 16384, "18": 18432, "20": 20480, "24": 24576, "32": 32768}'
+        NEW_ZRAM=$( "$JQ" -n --arg pn "$PRODUCT_NAME" --argjson zs "$ZRAM_SIZE" '{product_name: [$pn], zram_size: $zs}' )
+        TEMP_TARGET="${TARGET}.tmp"
+        "$JQ" --indent 4 --argjson new_zram "$NEW_ZRAM" '.zram += [$new_zram]' "$TARGET" > "$TEMP_TARGET"
+        if [ $? -eq 0 ] && [ -s "$TEMP_TARGET" ]
+        then
+            mv "$TEMP_TARGET" "$TARGET"
+            ui_print "- 成功！"
+        else
+            ui_print "- 修改失败！"
+            ui_print "- 跳过修改！"
+            rm -f "$TEMP_TARGET"
+            rm -f "$TARGET"
+    fi
+else
+    ui_print "- 未找到文件！"
+    ui_print "- 跳过修改！"
+fi
