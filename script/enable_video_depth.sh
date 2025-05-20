ce385687db578c5b3c639cb0f0c49ce5e6949345
hiruocha <hiruocha@gmail.com>
2025-05-21 04:36:19 +0800

    style(script): 微调代码风格，统一变量名为大写

--- script/enable_video_depth.sh
+++ script/enable_video_depth.sh
@@ -1,21 +1,21 @@
 ui_print "- 正在添加视频景深壁纸……"
 
-file=/product/media/wallpaper/sysui_editor_wallpaper_group/enable_video_depth
+FILE=/product/media/wallpaper/sysui_editor_wallpaper_group/enable_video_depth
 
-if [ -e "$OLD_MODPATH"/system"$file" ]
+if [ -e "$OLD_MODPATH"/system"$FILE" ]
 then
-    ui_print "- 已找到旧模块文件 $(basename "$file")，复制中……"
-    mkdir -p "$(dirname "$MODPATH"/system"$file")"
-    cp "$OLD_MODPATH"/system"$file" "$MODPATH"/system"$file"
+    ui_print "- 已找到旧模块文件 $(basename "$FILE")，复制中……"
+    mkdir -p "$(dirname "$MODPATH"/system"$FILE")"
+    cp "$OLD_MODPATH"/system"$FILE" "$MODPATH"/system"$FILE"
     ui_print "- 成功！"
-elif [ -e "$file" ]
+elif [ -e "$FILE" ]
 then
-    ui_print "- 已找到文件 $(basename "$file")，无需修改！"
+    ui_print "- 已找到文件 $(basename "$FILE")，无需修改！"
 elif echo "$VERSION" | grep -q '^OS2'
 then
-    ui_print "- 未找到文件 $(basename "$file")，创建中……"
-    mkdir -p "$(dirname "$MODPATH"/system"$file")"
-    touch  "$MODPATH"/system"$file"
+    ui_print "- 未找到文件 $(basename "$FILE")，创建中……"
+    mkdir -p "$(dirname "$MODPATH"/system"$FILE")"
+    touch  "$MODPATH"/system"$FILE"
     ui_print "- 成功！"
 else
     ui_print "- 非 HyperOS 2.0！"
--- script/unlock_cn_gms.sh
+++ script/unlock_cn_gms.sh
@@ -1,21 +1,21 @@
 ui_print "- 正在去除国行系统 GMS 限制……"
 
-file=/product/etc/permissions/cn.google.services.xml
+FILE=/product/etc/permissions/cn.google.services.xml
 
-if [ -e "$OLD_MODPATH"/system"$file" ]
+if [ -e "$OLD_MODPATH"/system"$FILE" ]
 then
-    ui_print "- 已找到旧模块文件 $(basename "$file")，复制中……"
-    mkdir -p "$(dirname "$MODPATH"/system"$file")"
-    cp "$OLD_MODPATH"/system"$file" "$MODPATH"/system"$file"
+    ui_print "- 已找到旧模块文件 $(basename "$FILE")，复制中……"
+    mkdir -p "$(dirname "$MODPATH"/system"$FILE")"
+    cp "$OLD_MODPATH"/system"$FILE" "$MODPATH"/system"$FILE"
     ui_print "- 成功！"
-elif [ -e "$file" ]
+elif [ -e "$FILE" ]
 then
-    ui_print "- 已找到文件 $(basename "$file")，修改中……"
-    target="$MODPATH"/system"$file"
-    mkdir -p $(dirname "$target")
-    cp -f "$file" "$target"
-    sed -i '/cn.google.services/d' "$target"
-    sed -i '/services_updater/d' "$target"
+    ui_print "- 已找到文件 $(basename "$FILE")，修改中……"
+    TARGET="$MODPATH"/system"$FILE"
+    mkdir -p "$(dirname "$TARGET")"
+    cp -f "$FILE" "$TARGET"
+    sed -i '/cn.google.services/d' "$TARGET"
+    sed -i '/services_updater/d' "$TARGET"
     ui_print "- 成功！"
 else
     ui_print "- 未找到文件，可能是非国行系统！"
